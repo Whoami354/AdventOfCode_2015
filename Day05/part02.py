48f8955caf602f9d10e6b97fad37855b5dbd6c0b
Whoami354 <74734994+Whoami354@users.noreply.github.com>
2023-03-06 13:51:59 +0100

    Teil 2 von Tag 5 zu Ende gemachen

--- Day05/part02.py
+++ Day05/part02.py
@@ -3,12 +3,12 @@ import re
 lines = open("input", "r", encoding='utf-8').read().strip().split('\n')
 counter = 0
 
-for i in lines:
+for actualString in lines:
     isNice = False
     isTwoLetters = False
-    if any([i[j] == i[j + 2] for j in range(len(i) - 2)]):
+    if any([actualString[j] == actualString[j + 2] for j in range(len(actualString) - 2)]):
         isTwoLetters = True
-    if any([(i.count(i[k:k + 2]) >= 2) for k in range(len(i) - 2)]):
+    if any([(actualString.count(actualString[k:k + 2]) >= 2) for k in range(len(actualString) - 2)]):
         isNice = True
     if isNice and isTwoLetters:
         counter += 1
