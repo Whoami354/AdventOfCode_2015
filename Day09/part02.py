79d65ed911d90d48cba62eae43f3fc1a944ba3ba
Whoami354 <74734994+Whoami354@users.noreply.github.com>
2023-03-14 18:08:05 +0100

    Tag 9 gemacht

---
+++ Day09/part02.py
@@ -0,0 +1,29 @@
+lines = input = open("input", "r", encoding='utf-8').read().strip().split("\n")
+import itertools
+
+#lines = ['London to Dublin = 464', 'London to Belfast = 518', 'Dublin to Belfast = 141']
+distances = {}
+values = []
+
+for line in lines:
+    values = line.split(" ")
+    origin, destination, distance = values[0], values[2], int(values[4])
+    distances[(origin, destination)] = distance
+    distances[(destination, origin)] = distance
+
+locations = list(set(itertools.chain.from_iterable(distances.keys())))
+
+# initialize shortest distance to infinity
+highest_distance = float("-inf")
+
+# find the shortest distance by trying all possible routes
+for route in itertools.permutations(locations):
+    # calculate the total distance of the route
+    total_distance = 0
+    for i in range(len(route) - 1):
+        total_distance += distances.get((route[i + 1], route[i]), 0)
+    # update the shortest distance if this route is shorter
+    if total_distance > highest_distance and total_distance != 0:
+        highest_distance = total_distance
+
+print(highest_distance)
