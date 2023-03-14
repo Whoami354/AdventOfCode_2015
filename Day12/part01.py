b75f19835fc08e328437852e7529fe60f4859ff3
Whoami354 <74734994+Whoami354@users.noreply.github.com>
2023-03-14 19:24:54 +0100

    Tag 12 gemacht

--- Day12/part01.py
+++ Day12/part01.py
@@ -1,23 +1,22 @@
 import json
+
 lines = input = open("input", "r", encoding='utf-8').read().strip()
+
+
 def sum_numbers(json_str):
     total_sum = 0
     data = json.loads(json_str)
-
     if isinstance(data, int):
         return data
-
     if isinstance(data, list):
         for item in data:
             total_sum += sum_numbers(json.dumps(item))
-
     if isinstance(data, dict):
         for key, value in data.items():
             if isinstance(value, int):
                 total_sum += value
             else:
                 total_sum += sum_numbers(json.dumps(value))
-
     return total_sum
 
 print(sum_numbers(lines))
