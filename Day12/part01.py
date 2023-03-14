033d268c00ab3276d3fb1014cdbf16b3b24c9b2e
Whoami354 <74734994+Whoami354@users.noreply.github.com>
2023-03-14 19:19:17 +0100

    Tag 12 gemacht

---
+++ Day12/part01.py
@@ -0,0 +1,23 @@
+lines = input = open("input", "r", encoding='utf-8').read().strip()
+import json
+def sum_numbers(json_str):
+    total_sum = 0
+    data = json.loads(json_str)
+
+    if isinstance(data, int):
+        return data
+
+    if isinstance(data, list):
+        for item in data:
+            total_sum += sum_numbers(json.dumps(item))
+
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(value, int):
+                total_sum += value
+            else:
+                total_sum += sum_numbers(json.dumps(value))
+
+    return total_sum
+
+print(sum_numbers(lines))
---
+++ Day12/part02.py
@@ -0,0 +1,22 @@
+lines = input = open("input", "r", encoding='utf-8').read().strip()
+
+import json
+
+def sum_numbers(obj):
+    if isinstance(obj, int):
+        return obj
+    elif isinstance(obj, list):
+        return sum(sum_numbers(elem) for elem in obj)
+    elif isinstance(obj, dict):
+        if "red" in obj.values():
+            return 0
+        return sum(sum_numbers(value) for value in obj.values())
+    else:
+        return 0
+
+
+json_obj = json.loads(lines)
+
+total_sum = sum_numbers(json_obj)
+
+print(total_sum)
