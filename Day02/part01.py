fd9649b8c80f2403bc0027c0c05d6010c233ab28
Whoami354 <fabian.gregory98@hotmail.de>
2023-04-17 12:43:31 +0200

    refactoring

---
+++ Day02/part01.py
@@ -0,0 +1,14 @@
+lines = open("input", "r", encoding='utf-8').read().strip().split('\n')
+
+sum_square = 0
+for i in lines:
+    value = i.split('x')
+    length = int(value[0])
+    width = int(value[1])
+    height = int(value[2])
+    length_width = length * width
+    width_height = width * height
+    height_length = length * height
+    sum_square += (2 * length_width) + (2 * width_height) + (2 * height_length) + min(length_width, width_height,height_length)
+
+print(sum_square)
---
+++ Day02/part02.py
@@ -0,0 +1,15 @@
+lines = open("input", "r", encoding='utf-8').read().strip().split('\n')
+
+sum_ribbon = 0
+for i in lines:
+    value = i.split('x')
+    length = int(value[0])
+    width = int(value[1])
+    height = int(value[2])
+    lw = 2 * length + 2 * width
+    lh = 2 * length + 2 * height
+    wh = 2 * width + 2 * height
+    ribbon_bow = length * width * height
+    sum_ribbon += min(lw, lh, wh) + ribbon_bow
+
+print(sum_ribbon)
