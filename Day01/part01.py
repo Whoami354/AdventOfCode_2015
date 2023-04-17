513e6fd83d5564639bd76b1653251eb79266bc36
Whoami354 <fabian.gregory98@hotmail.de>
2023-04-17 12:19:26 +0200

    refactoring

--- Day01/part01.py
+++ Day01/part01.py
@@ -1,10 +1,3 @@
 lines = open("input", "r", encoding='utf-8').read().strip()
 
-floor = 0
-for i in lines:
-    if i == '(':
-        floor += 1
-    elif i == ')':
-        floor -= 1
-
 print(lines.count('(') - lines.count(')'))
